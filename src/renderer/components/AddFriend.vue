a3145e6095e5501ce1db9b9a7bfd03b945d5153b
damly <8051263@qq.com>
2017-12-26 08:31:44 +0800

    修改

--- src/renderer/components/AddFriend.vue
+++ src/renderer/components/AddFriend.vue
@@ -3,91 +3,33 @@
         background: #f5f7f9;
         width: 100%;
         height: 100%;
-    }
-
-    .add-layout-left {
-        height: 440px;
-    }
-
-    .add-layout-right {
-        height: 100%;
-        padding: 10px;
+        padding-left: 5px;
+        padding-right: 5px;
     }
 </style>
 <template>
     <div class="add-layout">
-        <Row type="flex">
-            <Col span="6" class="add-layout-left" style="padding-left: 5px">
-            <div>
-                <Table height="310" border :columns="helloColumns" :data="helloData" no-data-text="暂无验证内容"></Table>
-                <div>
-                    <Input style="margin-top: 10px" type="textarea" v-model="formHello.hello" placeholder="请输入验证内容">
-                    <Icon type="person" slot="prepend"></Icon>
-                    </Input>
-                </div>
-                <div>
-                    <Button style="margin-top: 10px" size="small" @click="handleAddSubmit()">
-                        添加验证内容
-                    </Button>
-                </div>
-                <div>
-                    <Checkbox v-model="single" style="margin-top: 5px">随机发送验证内容</Checkbox>
-                </div>
-            </div>
-            </Col>
-            <Col span="18" class="add-layout-right">
-            <Tabs>
-                <TabPane label="导入添加好友" :disabled="disableAddFromInput">
-                    <AddFromInput></AddFromInput>
-                </TabPane>
-                <TabPane label="从好友群添加" :disabled="disableAddFromGroup">
-                    <AddFromGroup></AddFromGroup>
-                </TabPane>
-            </Tabs>
-            </Col>
-        </Row>
+        <Tabs>
+            <TabPane label="导入添加好友" :disabled="disableAddFromInput">
+                <AddFromInput></AddFromInput>
+            </TabPane>
+            <TabPane label="从好友群添加" :disabled="disableAddFromGroup">
+                <AddFromGroup></AddFromGroup>
+            </TabPane>
+        </Tabs>
     </div>
 </template>
 <script>
   import { onEvent } from './Event'
   import AddFromInput from './AddFromInput.vue'
   import AddFromGroup from './AddFromGroup.vue'
+
   export default {
-    components: { AddFromInput, AddFromGroup },
+    components: {AddFromInput, AddFromGroup},
     data () {
       return {
         disableAddFromInput: false,
-        disableAddFromGroup: false,
-        formHello: {
-          hello: ''
-        },
-        helloColumns: [
-          {
-            title: '验证内容',
-            key: 'hello'
-          },
-          {
-            title: '操作',
-            key: 'action',
-            width: 80,
-            render: (h, params) => {
-              return h('div', [
-                h('Button', {
-                  props: {
-                    type: 'text',
-                    size: 'small'
-                  },
-                  on: {
-                    click: () => {
-                      this.handleRemoveText(params.index)
-                    }
-                  }
-                }, '删除')
-              ])
-            }
-          }
-        ],
-        helloData: []
+        disableAddFromGroup: false
       }
     },
     created () {
@@ -112,19 +54,6 @@
       })
     },
     methods: {
-      handleRemoveText (index) {
-        this.helloData.splice(index, 1)
-      },
-      handleAddSubmit (name) {
-        let that = this
-        if (that.formHello.hello) {
-          let item = {
-            hello: that.formHello.hello
-          }
-          that.helloData.push(item)
-          that.formHello.hello = ''
-        }
-      }
     }
   }
 </script>
--- src/renderer/components/SettingView.vue
+++ src/renderer/components/SettingView.vue
@@ -18,53 +18,79 @@
 </style>
 <template>
     <div class="setting-layout">
-        <Row type="flex" >
+        <Row type="flex">
             <Col span="7" class="setting-layout-left">
+                <Button style="margin-top: 5px;margin-bottom: 5px" size="small"  @click="handleShowAddAccountModal()">添加账号</Button>
                 <Table height="320" border :columns="columns"
                        :data="settings.accounts"
                        no-data-text="您还未添加微信账号"
-                >
-                </Table>
-                <Input type="text" v-model="formAccount.account" placeholder="账号" style="margin-top: 5px">
-                    <Icon type="person" slot="prepend"></Icon>
-                </Input>
-                <Input type="password" v-model="formAccount.password" placeholder="密码" style="margin-top: 5px">
-                    <Icon type="locked" slot="prepend"></Icon>
-                </Input>
-                <Button type="primary" @click="handleAddSubmit('formInline')"  style="margin-top: 5px">添加账号</Button>
+                />
+                <div style="margin-top: 5px;margin-bottom: 5px"> 账号登录间隔时间:
+                    <InputNumber :max="3600" :min="1"
+                                 v-model="settings.loginTimer"
+                                 @on-change="handleLoginTimerChange">
+                    </InputNumber> 秒
+                </div>
             </Col>
             <Col span="17" class="setting-layout-right">
-            <Card style="margin-top: 10px">
-                <Form :model="settings" :label-width="120">
-                    <FormItem label="账号登录间隔">
-                        <InputNumber :max="3600" :min="1"
-                                     v-model="settings.loginTimer"
-                                     @on-change="handleLoginTimerChange">
-                        </InputNumber>
-                        秒
-                    </FormItem>
-                    <FormItem label="操作执行间隔">
-                        <InputNumber :max="3600" :min="1"
-                                     v-model="settings.optTimer"
-                                     @on-change="handleOptTimerChange">
-                        </InputNumber>
-                        秒
-                    </FormItem>
-                </Form>
-            </Card>
+            <div>
+                <Button style="margin-bottom: 5px" size="small" @click="handleShowAddHelloModal()">
+                    添加验证内容
+                </Button>
+                <Table height="200" border :columns="helloColumns"
+                       :data="settings.hellos"
+                       no-data-text="暂无验证内容"/>
+                <Checkbox v-model="settings.randomHello"
+                          @on-change="handleRandomHelloChange"
+                          style="margin-top: 5px;margin-bottom: 5px"
+                >随机发送验证内容
+                </Checkbox>
+                <div style="margin-top: 5px;margin-bottom: 5px"> 操作间隔时间:
+                    <InputNumber :max="3600" :min="1"
+                                 v-model="settings.optTimer"
+                                 @on-change="handleOptTimerChange">
+                    </InputNumber> 秒
+                </div>
+
+            </div>
             </Col>
         </Row>
+        <Modal v-model="addAccountModal"
+               title="添加微信账号"
+               ok-text="确定"
+               cancel-text="取消"
+               @on-ok="handleAddAccountSubmit">
+            <Form ref="formAccount" :model="formAccount" :label-width="120" :rules="ruleInline" inline>
+                <FormItem prop="account" label="登录账号">
+                    <Input type="text" v-model="formAccount.account"/>
+                </FormItem>
+                <FormItem prop="password" label="登录密码">
+                    <Input type="password" v-model="formAccount.password"/>
+                </FormItem>
+            </Form>
+        </Modal>
+        <Modal v-model="addHelloModal"
+               title="微信添加好友时验证内容"
+               ok-text="确定"
+               cancel-text="取消"
+               @on-ok="handleAddHelloSubmit">
+            <Input style="margin-top: 10px" type="textarea" v-model="formHello.hello" placeholder="请输入验证内容"/>
+        </Modal>
     </div>
 </template>
 <script>
   export default {
     data () {
       return {
+        addAccountModal: false,
         formAccount: {
           account: '',
           password: ''
         },
         ruleInline: {
+          hello: [
+            {required: true, message: '请输入验证内容.', trigger: 'blur'}
+          ],
           account: [
             {required: true, message: '请输入账号.', trigger: 'blur'}
           ],
@@ -106,10 +132,42 @@
             }
           }
         ],
+        addHelloModal: false,
+        formHello: {
+          hello: ''
+        },
+        helloColumns: [
+          {
+            title: '验证内容',
+            key: 'hello'
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 80,
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'text',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.handleRemoveHello(params.index)
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
         settings: {
           loginTimer: 10,
           optTimer: 10,
-          accounts: []
+          accounts: [],
+          hellos: [],
+          randomHello: false
         }
       }
     },
@@ -136,20 +194,41 @@
         this.settings.accounts.splice(index, 1)
         this.save()
       },
-      handleAddSubmit (name) {
-        let that = this
-        this.$refs[name].validate((valid) => {
-          if (valid) {
-            let item = {
-              account: that.formAccount.account,
-              password: that.formAccount.password
-            }
-            that.settings.accounts.push(item)
-            that.save()
-          } else {
-            this.$Message.error('输入不正确!')
-          }
-        })
+      handleShowAddAccountModal () {
+        this.addAccountModal = true
+      },
+      handleAddAccountSubmit () {
+        if (!this.formAccount.password || !this.formAccount.account) {
+          return
+        }
+        let item = {
+          account: this.formAccount.account,
+          password: this.formAccount.password
+        }
+        this.settings.accounts.push(item)
+        this.save()
+        this.formAccount.account = ''
+        this.formAccount.password = ''
+      },
+      handleRandomHelloChange () {
+        this.save()
+      },
+      handleShowAddHelloModal () {
+        this.addHelloModal = true
+      },
+      handleAddHelloSubmit () {
+        if (!this.formHello.hello) {
+          return
+        }
+        let item = {
+          hello: this.formHello.hello
+        }
+        this.settings.hellos.push(item)
+        this.save()
+      },
+      handleRemoveHello (index) {
+        this.settings.hellos.splice(index, 1)
+        this.save()
       }
     }
   }
